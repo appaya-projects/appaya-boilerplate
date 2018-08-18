87a24c497d165b4849486c8fb705eb17086af33e
mateuszszmytko <mateusz.szmytko@gmail.com>
2018-08-18 03:10:11 +0200

    styles refactoring #4rjpg

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,7 +2,6 @@ import { BehaviorBootstrap } from '@appaya/behavior';
 import { FormAjaxBehavior } from '@appaya/behavior/list';
 import { MyFormValidateBehavior } from './behaviors/my-form-validate.behavior';
 
-
 BehaviorBootstrap([
-	FormAjaxBehavior, MyFormValidateBehavior
+	FormAjaxBehavior, MyFormValidateBehavior,
 ]);
